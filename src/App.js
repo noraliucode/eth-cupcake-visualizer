f6e1b17ae9f18208500e052f5a7841e5f5807714
Nora Liu <noraliu0830@gmail.com>
2021-02-26 01:29:21 +0800

    feat: add sorting to addresses

--- src/App.js
+++ src/App.js
@@ -8,6 +8,18 @@ var web3 = new Web3('https://mainnet.infura.io/v3/83b98a98c5ca4761ac26ad2e8210df
 
 let blockInfos = [];
 
+const SOURCE_ADDRESSED_ARRAY = [
+  '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D',
+  '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f',
+  '0xf164fC0Ec4E93095b804a4795bBe1e041497b92a',
+  '0xD551234Ae421e3BCBA99A0Da6d736074f22192FF',
+  '0xdac17f958d2ee523a2206206994597c13d831ec7',
+  '0x1E0447b19BB6EcFdAe1e4AE1694b0C3659614e4e',
+  '0x4Ddc2D193948926D02f9B1fE9e1daa0718270ED5',
+  '0xfa52274dd61e1643d2205169732f29114bc240b3',
+  '0x6B175474E89094C44Da98b954EedeAC495271d0F',
+  ]
+
 const initBlock = async() => {
   const blockNumber = await web3.eth.getBlockNumber();
   for (var i = 0; i < 10; i++) {
@@ -41,6 +53,15 @@ const GetBlockEffect = (handler, interval) => {
   }, []);
   return () => clearInterval(intervalId);
 };
+
+const sortingToAddresses = async(transactions) => {
+  for (let i of transactions) {
+    const transaction = await web3.eth.getTransaction(i)
+    if(SOURCE_ADDRESSED_ARRAY.includes(transaction.to)) {
+      // count++
+    }
+  }
+}
 
 function App() {
   GetBlockEffect(getLatestBlocks, 10000)
